21c11196be26f58559cd874b547710ebd6d82483
Joao Ponte <60146735+a17694@users.noreply.github.com>
2024-01-14 22:35:11 +0000

    * codigo comentado

--- InterfaceAAD/Models/Interfaces/IBaseRepository.cs
+++ InterfaceAAD/Models/Interfaces/IBaseRepository.cs
@@ -1,22 +1,27 @@
 ﻿namespace InterfaceAAD.Models.Interfaces
 {
     /// <summary>
-    /// Interface defining basic repository operations for a specific model.
+    /// Defines the basic repository operations for a specific model.
     /// </summary>
     /// <typeparam name="M">The type of the model for which the repository is defined.</typeparam>
     internal interface IBaseRepository<M>
     {
+        /// <summary>
+        /// Saves an entity in the repository.
+        /// </summary>
+        /// <param name="entity">The entity to be saved.</param>
+        /// <returns>True if the save operation is successful, otherwise false.</returns>
         bool Save(M entity);
 
         /// <summary>
-        /// Gets an entity by its unique identifier.
+        /// Retrieves an entity by its unique identifier.
         /// </summary>
         /// <param name="id">The unique identifier of the entity.</param>
         /// <returns>The entity with the specified identifier, or null if not found.</returns>
         M GetById(int id);
 
         /// <summary>
-        /// Gets a list of all entities in the repository.
+        /// Retrieves a list of all entities in the repository.
         /// </summary>
         /// <returns>A list of entities.</returns>
         List<M> GetAll();
